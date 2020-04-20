5283abbc13287c50c212c4f1823a77dbdbaa1da8
Eric Wood <epwood1@gmail.com>
2020-04-19 18:43:23 -0700

    migrations

---
+++ data/migrations/20200419180825_tables.js
@@ -0,0 +1,52 @@
+
+exports.up = function(knex) {
+    return knex.schema
+        .createTable("projects", table => {
+            table.increments()
+            table.text("project_name").notNullable();
+            table.text("project_description")
+            table.boolean("complete").notNullable().defaultTo(0);
+        })
+        .createTable("tasks", table => {
+            table.increments()
+            table.text("task_description").notNullable();
+            table.text("task_notes");
+            table.boolean("compelete").notNullable().defaultTo(0);
+            table.integer("project_id")
+                .notNullable()
+                .unsigned()
+                .references("id")
+                .inTable("projects")
+                .onDelete("CASCADE")
+                .onUpdate("CASCADE")
+        })
+        .createTable("resources", table => {
+            table.increments()
+            table.text("resource_name").notNullable();
+            table.text("resource_description").notNullable();
+        })
+        .createTable("project_resource", table => {
+            table.integer("project_id")
+                .notNullable()
+                .unsigned()
+                .references("id")
+                .inTable("projects")
+                .onDelete("CASCADE")
+                .onUpdate("CASCADE")
+            table.integer("resource_id")
+                .notNullable()
+                .unsigned()
+                .references("id")
+                .inTable("resources")
+                .onDelete("CASCADE")
+                .onUpdate("CASCADE")
+        })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists("projects")
+        .dropTableIfExists("tasks")
+        .dropTableIfExists("resources")
+        .dropTableIfExists("project_resources")
+};
